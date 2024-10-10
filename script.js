f9d8bf32a9d43287890f25c47dbc3f86f788d1c5
Parvez Coder <ParvezCoder786@gmail.com>
2024-10-10 23:27:02 +0500

    reverse back to basic contact

--- script.js
+++ script.js
@@ -49,19 +49,24 @@ function openAndDownloadCV() {
     });
 
 
-    function downloadBtn() {
-        alert("CV download Successfully")
+    async function sendEmail() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('msg').value;
+    
+        var templateParams = {
+            from_name: name,
+            from_email: email,
+            message: message,
+            to_email: 'parvezcoder786@gmail.com'  // Your email
+        };
+    
+        try {
+            let response = await emailjs.send('service_xvky2xu', 'template_j9wxblb', templateParams, "C4b5-1sg2WE1pHNb2");
+            alert('Message sent successfully!');
+        } catch (error) {
+            alert('Failed to send the message. Please try again.');
+        }
     }
-        let submit = document.getElementById("submit")
-        submit.addEventListener("click", function () {
-            let name = document.getElementById("name")
-        let email = document.getElementById("email")
-        let msg = document.getElementById("msg")
-        if (email.value == "" || msg.value == "" ||  name.value == "" ) {
-            alert("please fill field")
-        }
-        else{
-            alert("thanks for contact")
-        }
-    })
+    
     
